91ee6a762b6da2411b042c24684bad3d3659d6bc
ks1ksi <ksi990302@gmail.com>
2023-12-15 17:44:39 +0900

    feat: 쿼리 임베딩 추가

---
+++ pages/api/getEmbedding.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import openai from '../../utils/openai';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { text } = req.body;
+
+    try {
+      const result = await openai.embeddings.create({
+        model: 'text-embedding-ada-002',
+        input: text,
+      });
+
+      res.status(200).json(result.data[0].embedding);
+    } catch (error) {
+      console.error('OpenAI embedding error:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
